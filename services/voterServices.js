bdcdbb79e133b4372267cb2074be0e6a7df4ab24
Doglanddog <jim.sievers@gmail.com>
2023-09-08 14:00:51 -0700

    add the fiest logging

--- services/voterServices.js
+++ services/voterServices.js
@@ -1,11 +1,52 @@
+
+const winston = require('winston');
 const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+};
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
 
 async function getMultiple(page = 1) {
   const offset = helper.getOffset(page, config.listPerPage);
   
   console.log('select data');
+
+  logger.log({
+    level: 'info',
+    message: 'Hello distributed log files!'
+  });
   
   const rows = await db.query(
     `SELECT CountyID, StateID, Status, County, Precinct, First, Last, Middle, Phone, email, BirthDate, RegDate, Party,
